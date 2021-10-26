41b91dd7475f410523bae35d6a2bb5b75016fb69
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2021-10-26 22:12:22 +0300

    Class renamed.

--- VehicleLibrary/Vehicle.cs
+++ VehicleLibrary/Vehicle.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace VehicleLibrary
 {
-    public class Repository
+    public class Vehicle
     {
         public int VehicleId { get; private set; }
         public string VehicleType { get; private set; }
         public DateTime EnterTime { get; private set; }
         public DateTime ExitTime { get; private set; }
 
-        public Repository(int vehicleId, string vehicleType, DateTime enterTime, DateTime exitTime)
+        public Vehicle(int vehicleId, string vehicleType, DateTime enterTime, DateTime exitTime)
         {
             VehicleId = vehicleId;
             VehicleType = vehicleType;
